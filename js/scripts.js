abbe0f9d8aa43942bd75cf92c4dca7a972908beb
Alexandra Holcombe and Minh Phuong <alexandra@>
2017-01-17 09:30:32 -0800

    Define business and user logic add countOutput function console.log results

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
 //Business
-  var countTo = 0;
-  var countBy = 0;
+var countTo = 0;
+var countBy = 0;
+
+var countOutput = function() {
+  for (var currentNumber = 0; currentNumber <= countTo; currentNumber += countBy) {
+    $("#output").append(currentNumber + ", ");
+    console.log(currentNumber);
+  };
+};
+
 
 
 //User
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-      countTo = parseInt($("input#count-to").val());
-      countBy = parseInt($("input#count-by").val());
-    });
+    countTo = parseInt($("input#count-to").val());
+    countBy = parseInt($("input#count-by").val());
+
+    console.log(countTo);
+    console.log(countBy);
+
+    countOutput();
+  });
 });
